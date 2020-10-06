e4dbb26aeaa336c62461ed10e421ad1627eec9a6
Andrei Timurlea <timurlea.andrei@isoftbet.com>
2020-10-06 18:31:31 +0300

    - Added lodash - Added login implementation

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,30 +1,37 @@
-import React, {useState} from "react";
+import React, {FunctionComponent, useContext, useState} from "react";
 import {Button, TextField} from "@material-ui/core";
+import ProductsStore from "../stores/products-store"
+import {observer} from "mobx-react";
 
-function LoginComponent(props:any){
-	const [logged, setLogged] = useState(false);
+export const LoginComponent: FunctionComponent = observer(() => {
+	const productsStore = useContext(ProductsStore);
+	const [user, setUser] = useState("");
+	const [password, setPassword] = useState("");
 
-		if(logged) {
-			return <div>
-				Hello, admin!
-			</div>;
-		} else {
-			return <div>
-				<TextField required id="standard-required" label="Required" defaultValue="type user" />
-				<TextField
-					id="standard-password-input"
-					label="Password"
-					type="password"
-					autoComplete="current-password"
-				/>
-				<Button variant="contained" color="primary" onClick={()=>{
-					console.log("click pe log");
-					setLogged(true);}
-				}>
-					Login
-				</Button>
-			</div>;
-		}
-}
+	if(productsStore.logedIn.get()) {
+		return <div>
+			Hello, {productsStore.user.get()}!
+		</div>;
+	} else {
+		return <div>
+			<TextField required id="standard-required" label="Required" defaultValue="type user"
+			           onChange={evt => setUser(evt.target.value)}/>
+			<TextField
+				id="standard-password-input"
+				label="Password"
+				type="password"
+				autoComplete="current-password"
+				onChange={evt => setPassword(evt.target.value)}
+			/>
+			<Button variant="contained" color="primary" onClick={()=>{
+				console.log("click pe log");
+				productsStore.login(user, password);
+			}
+			}>
+				Login
+			</Button>
+		</div>;
+	}
+});
 
 export default LoginComponent;
--- src/stores/products-store.tsx
+++ src/stores/products-store.tsx
@@ -1,12 +1,14 @@
-import {observable} from "mobx";
+import {action, observable} from "mobx";
 import productsJson from "../resources/products.json";
 import {createContext} from "react";
 import * as _ from "lodash";
 import {IProduct} from "./IProduct";
 
 class ProductsStore{
+	private LOGIN_API_URL = "http://atelierturbosuflante.ro/php/api.php";
 	@observable public products: IProduct[];
-
+	@observable public logedIn = observable.box(false);
+	@observable public user = observable.box('');
 	constructor() {
 		this.products = Object.values(productsJson) as IProduct[];
 		console.log("Products store created. Products: " + this.products.length);
@@ -17,6 +19,25 @@ class ProductsStore{
 			return p.id == id;
 		});
 		return prod;
+	}
+
+	@action
+	public login(user: string, password: string): boolean{
+		let requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ user: user, pass: password })
+		};
+		fetch(this.LOGIN_API_URL, requestOptions)
+		.then(response => response.json())
+		.then((data) => {
+			this.logedIn.set(data.length > 0);
+			if(this.logedIn.get()){
+				this.user.set(data[0].user);
+			}
+			console.log(data);
+		});
+		return this.logedIn.get();
 	}
 }
 
