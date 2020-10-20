5098d99c5c6dd55157e8410ba6f84adf483187a0
Andrei Timurlea <timurlea.andrei@isoftbet.com>
2020-10-20 21:52:59 +0300

    - updated ProductDetailComponent

--- src/components/ProductDetailComponent.tsx
+++ src/components/ProductDetailComponent.tsx
@@ -1,17 +1,39 @@
 import React, {useContext} from "react";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import ProductsStore from "../stores/products-store"
+import {TextField} from "@material-ui/core";
 
 function ProductDetailComponent(props: any) {
 	const productsStore = useContext(ProductsStore);
 	let { id } = useParams();
-	return (
-		<div>
-			<div>{productsStore.getProductById(id)?.name}</div>
-			<div>{productsStore.getProductById(id)?.description}</div>
-			<div>{productsStore.getProductById(id)?.price}</div>
-		</div>
-	);
+
+	function DescriptionComponent() {
+		return (
+			<div>
+				<div>
+					<TextField disabled id="standard-disabled" label={productsStore.getProductById(id)?.id}/>
+				</div>
+				<div>
+					<TextField disabled id="standard-disabled" label={productsStore.getProductById(id)?.name}/>
+				</div>
+				<div>
+					<TextField disabled id="sstandard-disabled" label={productsStore.getProductById(id)?.category}/>
+				</div>
+				<div>
+					<TextField disabled id="standard-disabled" label={productsStore.getProductById(id)?.image}/>
+				</div>
+				<div>
+					<TextField disabled id="standard-disabled" label={productsStore.getProductById(id)?.price}/>
+				</div>
+				<div>
+					<TextField disabled id="standard-disabled" label={productsStore.getProductById(id)?.description}/>
+				</div>
+			</div>
+		);
+	}
+
+	return <DescriptionComponent/>
+
 }
 
 export default ProductDetailComponent;
