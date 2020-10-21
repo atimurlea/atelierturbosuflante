e4806c68cd6c331b406d248e1d10722a60324e5c
Andrei Timurlea <timurlea.andrei@isoftbet.com>
2020-10-21 21:00:39 +0300

    - updated HomeComponent and ProductDetailComponent

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import turbina from '../images/turbina.jpg';
+import {makeStyles, Theme} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) => ({
+	root: {
+		position: 'absolute',
+		left: '50%',
+		top: '200px',
+		transform: 'translate(-50%, -50%)'
+	},
+}));
 
 function HomeComponent() {
+	const classes = useStyles();
 
 	return (
-		<span>
-			<img src={turbina} alt="img"/>
-			Bine ati venit la Atelier Turbosuflante! By Razvan :)
-		</span>
+		<div className={classes.root}>
+			<div>
+				<img src={turbina} alt="img"/>
+			</div>
+			<div>
+				Bine ati venit la Atelier Turbosuflante! By Razvan :)
+			</div>
+		</div>
 
 	);
 }
--- src/components/ProductDetailComponent.tsx
+++ src/components/ProductDetailComponent.tsx
@@ -2,33 +2,41 @@ import React, {useContext} from "react";
 import {useParams} from "react-router-dom";
 import ProductsStore from "../stores/products-store"
 import {List, ListItem, ListItemText} from "@material-ui/core";
+import {makeStyles, Theme} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) => ({
+	root: {
+		position: 'absolute',
+		left: '50%',
+		top: '400px',
+		transform: 'translate(-50%, -50%)'
+	},
+}));
 
 function ProductDetailComponent(props: any) {
+	const classes = useStyles();
 	const productsStore = useContext(ProductsStore);
 	let { id } = useParams();
 
 	function DescriptionComponent() {
 		let product = productsStore.getProductById(id);
 		return (
-			<div>
+			<div className={classes.root}>
 				<List component="nav" aria-label="test">
 					<ListItem button>
-						<ListItemText primary={product?.id} />
+						<img src={product?.image} alt="img"/>
 					</ListItem>
 					<ListItem button>
-						<ListItemText primary={product?.name} />
+						<ListItemText primary={'Name: '+product?.name} />
 					</ListItem>
 					<ListItem button>
-						<ListItemText primary={product?.category} />
+						<ListItemText primary={'Category: '+product?.category} />
 					</ListItem>
 					<ListItem button>
-						<ListItemText primary={product?.image} />
+						<ListItemText primary={'Price: '+product?.price} />
 					</ListItem>
 					<ListItem button>
-						<ListItemText primary={product?.price} />
-					</ListItem>
-					<ListItem button>
-						<ListItemText primary={product?.description} />
+						<ListItemText primary={'Description: '+product?.description} />
 					</ListItem>
 				</List>
 			</div>
